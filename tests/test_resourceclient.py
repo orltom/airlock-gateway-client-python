365631cdfb0511a2886a34c6c313fcfdffac9625
Orlando Tomas <orlando.tomas@ergon.ch>
2020-11-21 22:31:45 +0100

    Enable pytest for github actions

--- tests/test_resourceclient.py
+++ tests/test_resourceclient.py
@@ -1,16 +1,32 @@
 from client.httpclient import HttpClient
-from client.resourceclient import AirlockSession
+from client.resourceclient import AirlockSession, AuthenticationClient
 from requests import Response
+from pytest import raises
 
 
 class DummyHttpClient(HttpClient):
+    def __init__(self, status_code):
+        self.status_code = status_code
+
     def post(self, path: str, data=None, headers: dict = HttpClient.DEFAULT_HEADERS):
         response = Response()
-        response.status_code = 200
+        response.status_code = self.status_code
         return response
 
 
 def test_terminate():
-    session = AirlockSession(DummyHttpClient())
+    session = AirlockSession(DummyHttpClient(200))
     response = session.terminate()
     assert response.status_code == 200
+
+
+def test_authentication_successful():
+    authentication = AuthenticationClient("test", DummyHttpClient(200))
+    airlock_session = authentication.create()
+    assert type(airlock_session) == AirlockSession
+
+
+def test_authentication_failed():
+    authentication = AuthenticationClient("test", DummyHttpClient(500))
+    with raises(Exception):
+        authentication.create()
